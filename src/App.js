a39595218af2aa0f37077b0542074ea86a5b2964
nariya <nariya@shota-2.local>
2020-10-07 19:30:55 +0900

    プロフィール追加

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './avatar.jpg';
 import './App.css';
-
+import { Button } from '@material-ui/core';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>金城　功弥</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          1984年　４月　２８日
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
